395bea4d09e2f18b0bcfaf75a879c78f71636463
OsipchikMinsk <Osipchik@tut.by>
2019-11-14 20:27:25 +0300

    Pull Request Ex

--- src/main/java/by/epam/sphere/Main.java
+++ src/main/java/by/epam/sphere/Main.java
@@ -11,19 +11,17 @@ public class Main {
     static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
     public static void main(String[] args) {
 
-  /*      Point p1 = new Point(100,0,0);
+        Point p1 = new Point(100,0,0);
         SphereLogic sphereLogic = new SphereLogic();
         Sphere sphere= new Sphere(p1,20);
-        System.out.println(sphereLogic.isSphereCrossesAxis(sphere));
+        //System.out.println(sphereLogic.isSphereCrossesAxis(sphere));
         System.out.println(sphereLogic.isSphere(sphere));
         System.out.println(sphereLogic.calcVolumeRatio(sphere));
-        LOGGER.info("Работа завершена");*/
         SphereCreator sphereCreator = new SphereCreator();
         sphereCreator.createSphereFromFile();
-
-        /*System.out.println(sphereLogic.calcSphereVolume(sphere));
+        System.out.println(sphereLogic.calcSphereVolume(sphere));
         System.out.println(sphereLogic.calculateSphereArea(sphere));
-        System.out.println(Math.PI);
-*/
-    }
+        LOGGER.info("Работа завершена");
+
+        }
 }

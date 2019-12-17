3be5c92d1a87701db2e6f016531a4add73d4d182
OsipchikMinsk <Osipchik@tut.by>
2019-12-17 15:43:54 +0300

    Added Try With Resources

--- src/test/java/by/epam/sphere/logic/SphereLogicTest.java
+++ src/test/java/by/epam/sphere/logic/SphereLogicTest.java
@@ -4,54 +4,53 @@ import by.epam.sphere.entity.Point;
 import by.epam.sphere.entity.Sphere;
 import org.junit.Assert;
 import org.junit.Before;
-
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class SphereLogicTest {
+
     private Point point = new Point(5,5,5);
     private Sphere  sphere;
     private SphereLogic sphereLogic = new SphereLogic();
     @Before
     public void initSphere(){
        sphere = new Sphere(point,10);
-
     }
 
 
-    @org.junit.Test
+    @Test
     public void isRadiusNumber() throws Exception {
         double actual = sphere.getRadius();
         double expected = 10;
         Assert.assertEquals(expected,actual,0);
     }
 
-    @org.junit.Test
+    @Test
     public void calculateSphereArea() throws Exception {
         double actual = sphereLogic.calculateSphereArea(sphere);
         double expected = 1256;
         Assert.assertEquals(expected,actual,0);
     }
 
-    @org.junit.Test
+    @Test
     public void calcSphereVolume() throws Exception {
         double actual = sphereLogic.calcSphereVolume(sphere);
         double expected = 3140;
         Assert.assertEquals(expected,actual,0);
     }
 
-    @org.junit.Test
+    @Test
     public void isSphere() throws Exception {
         boolean actual = sphereLogic.isSphere(sphere);
         Assert.assertTrue(actual);
     }
 
-    @org.junit.Test
+    @Test
     public void isSphereCrossesAxis() throws Exception {
         boolean actual = sphereLogic.isSphereCrossesAxis(sphere);
         Assert.assertTrue( actual);
     }
 
-    @org.junit.Test
+    @Test
     public void calcVolumeRatio() throws Exception {
         double actual = sphereLogic.calcVolumeRatio(sphere);
         double expected = 3;
